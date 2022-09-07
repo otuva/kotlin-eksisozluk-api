6f2f391fbdc406f67fc48bf998f66a26826dbee9
Onur Alp Akin <onralpakin@gmail.com>
2022-09-07 17:43:19 +0300

    add jsdoc handle anon-login response separately

--- src/main/kotlin/io/github/otuva/eksisozluk/models/EksiToken.kt
+++ src/main/kotlin/io/github/otuva/eksisozluk/models/EksiToken.kt
@@ -6,18 +6,32 @@ import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.*
 import kotlin.time.Duration.Companion.seconds
 
-fun deserializeAnonAuth(response: String): EksiToken {
-    val jsonElement = Json.parseToJsonElement(response)
+/**
+ * @param rank User rank. 0 is for anonymous users.
+ * @param accessToken Access token for API requests.
+ * @param tokenType Token type. Always "Bearer".
+ * @param expiresIn Token expiration time in seconds.
+ * @param refreshToken Refresh token for API requests. Only available for registered users.
+ * @param nick User nick. Only available for registered users.
+ * @param userId User ID. Only available for registered users.
+ * @param issuedAt Token issue time to handle expiration and refresh.
+ * @param expiresAt Time instant of when the token expires. Calculated by adding [expiresIn] to [issuedAt].
+ * */
+@Serializable
+data class EksiToken(
+    val rank: Int,
+    val accessToken: String,
+    val tokenType:String,
+    val expiresIn: Int,
+    val userId: Int?,
+    val refreshToken: String?,
+    val nick: String?,
+    val issuedAt: Instant = Clock.System.now(),
+    val expiresAt: Instant = issuedAt + expiresIn.seconds
+)
 
-    val success = jsonElement.jsonObject["Success"]!!.jsonPrimitive.boolean
-    val message = jsonElement.jsonObject["Message"]!!.jsonPrimitive.content
-    val actualData = jsonElement.jsonObject["Data"].toString()
-
-    return deserializeAuth(actualData)
-}
-
-fun deserializeAuth(response: String): EksiToken {
-    val jsonElement = Json.parseToJsonElement(response)
+fun deserializeAuth(json: String): EksiToken {
+    val jsonElement = Json.parseToJsonElement(json)
 
     val rank = jsonElement.jsonObject["rank"]!!.jsonPrimitive.int
     val accessToken = jsonElement.jsonObject["access_token"]!!.jsonPrimitive.content
@@ -37,25 +51,3 @@ fun deserializeAuth(response: String): EksiToken {
         userId=userId
     )
 }
-
-fun serializeEksiToken(token: EksiToken): String {
-    val json = Json {
-        prettyPrint = true
-        encodeDefaults = true
-    }
-
-    return json.encodeToString(EksiToken.serializer(), token)
-}
-
-@Serializable
-data class EksiToken(
-    val rank: Int,
-    val accessToken: String,
-    val tokenType:String,
-    val expiresIn: Int,
-    val userId: Int?,
-    val refreshToken: String?,
-    val nick: String?,
-    val issuedAt: Instant = Clock.System.now(),
-    val expiresAt: Instant = issuedAt + expiresIn.seconds
-)
