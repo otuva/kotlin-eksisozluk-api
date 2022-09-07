3e13ae005af234971b4642d89c23ddde2b121504
Onur Alp Akin <onralpakin@gmail.com>
2022-09-07 15:17:16 +0300

    add user classes

---
+++ src/main/kotlin/io/github/otuva/eksisozluk/models/User.kt
@@ -0,0 +1,30 @@
+package io.github.otuva.eksisozluk.models
+
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.int
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
+
+
+
+fun deserializeUserIdentifier(response: String): UserIdentifier {
+    val jsonElement = Json.parseToJsonElement(response)
+
+    val nick = jsonElement.jsonObject["Nick"]!!.jsonPrimitive.content
+    val id = jsonElement.jsonObject["Id"]!!.jsonPrimitive.int
+
+    return UserIdentifier(
+        nick = nick,
+        id = id
+    )
+}
+
+@Serializable
+data class UserIdentifier(
+    val nick: String,
+    val id: Int
+)
+
+//class User {
+//}
