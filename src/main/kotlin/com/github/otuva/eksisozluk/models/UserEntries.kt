f7542b579f0b0e4de544943f64525c20c366978c
Onur Alp Akin <onralpakin@gmail.com>
2022-09-11 22:49:55 +0300

    format equal sign

--- src/main/kotlin/com/github/otuva/eksisozluk/models/UserEntries.kt
+++ src/main/kotlin/com/github/otuva/eksisozluk/models/UserEntries.kt
@@ -65,18 +65,21 @@ data class TopicTitle(
 fun deserializeUserEntries(json: String): UserEntries {
     val jsonElement = Json.parseToJsonElement(json)
 
-    val pinnedEntry = if (jsonElement.jsonObject["PinnedEntry"] != null && jsonElement.jsonObject["PinnedEntry"] != JsonNull) deserializeTopic(jsonElement.jsonObject["PinnedEntry"].toString()) else null
+    val pinnedEntry =
+        if (jsonElement.jsonObject["PinnedEntry"] != null && jsonElement.jsonObject["PinnedEntry"] != JsonNull) deserializeTopic(
+            jsonElement.jsonObject["PinnedEntry"].toString()
+        ) else null
     val entries = jsonElement.jsonObject["Entries"]!!.jsonArray.map { deserializeCondensedTopic(it.toString()) }
     val pageCount = jsonElement.jsonObject["PageCount"]!!.jsonPrimitive.int
     val pageSize = jsonElement.jsonObject["PageSize"]!!.jsonPrimitive.int
     val pageIndex = jsonElement.jsonObject["PageIndex"]!!.jsonPrimitive.int
 
     return UserEntries(
-        pinnedEntry=pinnedEntry,
-        entries=entries,
-        pageCount=pageCount,
-        pageSize=pageSize,
-        pageIndex=pageIndex
+        pinnedEntry = pinnedEntry,
+        entries = entries,
+        pageCount = pageCount,
+        pageSize = pageSize,
+        pageIndex = pageIndex
     )
 }
 
@@ -94,8 +97,8 @@ fun deserializeCondensedTopic(json: String): CondensedTopic {
     val entry = deserializeEntry(jsonElement.jsonObject["Entry"]!!.toString())
 
     return CondensedTopic(
-        topicId=topicId,
-        entry=entry
+        topicId = topicId,
+        entry = entry
     )
 }
 
@@ -114,9 +117,9 @@ fun deserializeTopicId(json: String): TopicId {
     val title = jsonElement.jsonObject["Title"]!!.jsonPrimitive.content
 
     return TopicId(
-        id=id,
-        topicTitle=topicTitle,
-        title=title
+        id = id,
+        topicTitle = topicTitle,
+        title = title
     )
 }
 
@@ -134,7 +137,7 @@ fun deserializeTopicTitle(json: String): TopicTitle {
     val kind = jsonElement.jsonObject["Kind"]?.jsonPrimitive?.contentOrNull
 
     return TopicTitle(
-        title=title,
-        kind=kind
+        title = title,
+        kind = kind
     )
 }
