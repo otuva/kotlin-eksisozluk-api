570b5e15a762e240669911a6263b77d5e95e0326
Onur Alp Akin <onralpakin@gmail.com>
2022-09-11 14:44:59 +0300

    comment out serializable

--- src/main/kotlin/com/github/otuva/eksisozluk/responses/TopicResponse.kt
+++ src/main/kotlin/com/github/otuva/eksisozluk/responses/TopicResponse.kt
@@ -3,13 +3,13 @@ package com.github.otuva.eksisozluk.responses
 import com.github.otuva.eksisozluk.BadTopicException
 import com.github.otuva.eksisozluk.models.Topic
 import com.github.otuva.eksisozluk.models.deserializeTopic
-import kotlinx.serialization.Serializable
+// import kotlinx.serialization.json.*
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.boolean
 import kotlinx.serialization.json.jsonObject
 import kotlinx.serialization.json.jsonPrimitive
 
-@Serializable
+// @Serializable
 data class TopicResponse(
     val success: Boolean?,
     val message: String?,
