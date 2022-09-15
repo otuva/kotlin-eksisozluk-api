fc741ada5a3eea43fd28d25d07514bb1ccd6581e
Onur Alp Akin <onralpakin@gmail.com>
2022-09-15 13:28:52 +0300

    use kotlin serialization and add error handling

--- src/main/kotlin/com/github/otuva/eksisozluk/responses/TopicResponse.kt
+++ src/main/kotlin/com/github/otuva/eksisozluk/responses/TopicResponse.kt
@@ -2,33 +2,16 @@ package com.github.otuva.eksisozluk.responses
 
 import com.github.otuva.eksisozluk.BadTopicException
 import com.github.otuva.eksisozluk.models.Topic
-import com.github.otuva.eksisozluk.models.deserializeTopic
-// import kotlinx.serialization.json.*
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.boolean
-import kotlinx.serialization.json.jsonObject
-import kotlinx.serialization.json.jsonPrimitive
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
-// @Serializable
+@Serializable
 data class TopicResponse(
-    val success: Boolean?,
-    val message: String?,
-    val data: Topic
-)
-
-fun deserializeTopicResponse(json: String): TopicResponse {
-    val jsonElement = Json.parseToJsonElement(json)
-
-    val success = jsonElement.jsonObject["Success"]?.jsonPrimitive?.boolean
-    val message = jsonElement.jsonObject["Message"]?.jsonPrimitive?.content
-
-    if (success != true && message != null) throw BadTopicException(message)
-
-    val data = deserializeTopic(jsonElement.jsonObject["Data"].toString())
-
-    return TopicResponse(
-        success = success,
-        message = message,
-        data = data
-    )
+    @SerialName("Success") val success: Boolean,
+    @SerialName("Message") val message: String?,
+    @SerialName("Data") val data: Topic?
+) {
+    init {
+        if (!success) throw BadTopicException(message!!)
+    }
 }
