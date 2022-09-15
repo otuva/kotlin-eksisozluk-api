574cf1283b8646200adee926a0781e0f3871d84f
Onur Alp Akin <onralpakin@gmail.com>
2022-09-15 13:31:03 +0300

    use kotlin serialization

--- src/main/kotlin/com/github/otuva/eksisozluk/models/User.kt
+++ src/main/kotlin/com/github/otuva/eksisozluk/models/User.kt
@@ -1,7 +1,8 @@
 package com.github.otuva.eksisozluk.models
 
 import kotlinx.datetime.LocalDateTime
-import kotlinx.serialization.json.*
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
 /**
  * Represents a user
@@ -16,14 +17,15 @@ import kotlinx.serialization.json.*
  *
  * @see UserInfo
  * */
+@Serializable
 data class User(
-    val userInfo: UserInfo,
-    val badges: List<Badge>,
-    val hasEntryUsedOnSeyler: Boolean,
-    val followerCount: Int,
-    val followingsCount: Int,
-    val picture: String?,
-    val pinnedEntry: Topic?
+    @SerialName("UserInfo") val userInfo: UserInfo,
+    @SerialName("Badges") val badges: List<Badge>,
+    @SerialName("HasEntryUsedOnSeyler") val hasEntryUsedOnSeyler: Boolean,
+    @SerialName("FollowerCount") val followerCount: Int,
+    @SerialName("FollowingsCount") val followingsCount: Int,
+    @SerialName("Picture") val picture: String?,
+    @SerialName("PinnedEntry") val pinnedEntry: Topic?
 )
 
 /**
@@ -32,9 +34,10 @@ data class User(
  * @param nick User's nick
  * @param id User's internal id
  * */
+@Serializable
 data class UserIdentifier(
-    val nick: String,
-    val id: Int
+    @SerialName("Nick") val nick: String,
+    @SerialName("Id") val id: Int
 )
 
 /**
@@ -52,34 +55,35 @@ data class UserIdentifier(
  * @param standingQueueNumber User's standing queue number note that this will be 0 for authors
  *
  * */
+@Serializable
 data class UserInfo(
-    val userIdentifier: UserIdentifier,
-    val remainingInvitation: Int,
-    val twitterScreenName: String?,
-    val facebookProfileUrl: String?,
-    val facebookScreenName: String?,
-    val instagramScreenName: String?,
-    val instagramProfileUrl: String?,
-    val karma: Karma?,
-    val entryCounts: UserEntryCounts,
-    val lastEntryDate: LocalDateTime?,
-    val standingQueueNumber: Int,
-    val hasAnyPendingInvitation: Boolean,
-    val isBuddy: Boolean,
-    val isBlocked: Boolean,
-    val isFollowed: Boolean,
-    val isCorporate: Boolean,
-    val isDeactivated: Boolean,
-    val isKarmaShown: Boolean,
-    val isCaylak: Boolean,
-    val isIndexTitlesBlocked: Boolean,
-    val note: String?,
-    val cursePeriod: CursePeriod?,
-    val isCursed: Boolean,
-    val isBanned: Boolean,
-    val displayTwitterProfile: Boolean,
-    val displayFacebookProfile: Boolean,
-    val displayInstagramProfile: Boolean,
+    @SerialName("UserIdentifier") val userIdentifier: UserIdentifier,
+    @SerialName("RemainingInvitation") val remainingInvitation: Int,
+    @SerialName("TwitterScreenName") val twitterScreenName: String?,
+    @SerialName("FacebookProfileUrl") val facebookProfileUrl: String?,
+    @SerialName("FacebookScreenName") val facebookScreenName: String?,
+    @SerialName("InstagramScreenName") val instagramScreenName: String?,
+    @SerialName("InstagramProfileUrl") val instagramProfileUrl: String?,
+    @SerialName("Karma") val karma: Karma?,
+    @SerialName("EntryCounts") val entryCounts: UserEntryCounts,
+    @SerialName("LastEntryDate") val lastEntryDate: LocalDateTime?,
+    @SerialName("StandingQueueNumber") val standingQueueNumber: Int,
+    @SerialName("HasAnyPendingInvitation") val hasAnyPendingInvitation: Boolean,
+    @SerialName("IsBuddy") val isBuddy: Boolean,
+    @SerialName("IsBlocked") val isBlocked: Boolean,
+    @SerialName("IsFollowed") val isFollowed: Boolean,
+    @SerialName("IsCorporate") val isCorporate: Boolean,
+    @SerialName("IsDeactivated") val isDeactivated: Boolean,
+    @SerialName("IsKarmaShown") val isKarmaShown: Boolean,
+    @SerialName("IsCaylak") val isCaylak: Boolean,
+    @SerialName("IsIndexTitlesBlocked") val isIndexTitlesBlocked: Boolean,
+    @SerialName("Note") val note: String?,
+    @SerialName("CursePeriod") val cursePeriod: CursePeriod?,
+    @SerialName("IsCursed") val isCursed: Boolean,
+    @SerialName("IsBanned") val isBanned: Boolean,
+    @SerialName("DisplayTwitterProfile") val displayTwitterProfile: Boolean,
+    @SerialName("DisplayFacebookProfile") val displayFacebookProfile: Boolean,
+    @SerialName("DisplayInstagramProfile") val displayInstagramProfile: Boolean,
 )
 
 /**
@@ -88,9 +92,10 @@ data class UserInfo(
  * @param from Curse start date
  * @param to Curse end date
  * */
+@Serializable
 data class CursePeriod(
-    val from: LocalDateTime,
-    val to: LocalDateTime
+    @SerialName("From") val from: LocalDateTime,
+    @SerialName("To") val to: LocalDateTime
 )
 
 /**
@@ -101,11 +106,12 @@ data class CursePeriod(
  * @param lastWeek Number of entries written in the last week
  * @param today Number of entries written today
  * */
+@Serializable
 data class UserEntryCounts(
-    val total: Int,
-    val lastMonth: Int,
-    val lastWeek: Int,
-    val today: Int
+    @SerialName("Total") val total: Int,
+    @SerialName("LastMonth") val lastMonth: Int,
+    @SerialName("LastWeek") val lastWeek: Int,
+    @SerialName("Today") val today: Int
 )
 
 /**
@@ -116,9 +122,10 @@ data class UserEntryCounts(
  *
  * @see [KarmaName]
  * */
+@Serializable
 data class Karma(
-    val name: KarmaName,
-    val value: Int
+    @SerialName("Name") val name: KarmaName,
+    @SerialName("Value") val value: Int
 )
 
 /**
@@ -133,15 +140,16 @@ data class Karma(
  * @param selected <Useless> | Unknown. It is always false for all badges. Could be whether user chose to display on profile
  * @param showInactive <Useless> | Unknown. It is always false for all badges
  * */
+@Serializable
 data class Badge(
-    val id: Int,
-    val name: BadgeName,
-    val description: String?,
-    val imageUrl: String?,
-    val displayOrder: Int,
-    val owned: Boolean,
-    val selected: Boolean,
-    val showInactive: Boolean
+    @SerialName("Id") val id: Int,
+    @SerialName("Name") val name: BadgeName,
+    @SerialName("Description") val description: String,
+    @SerialName("ImageUrl") val imageUrl: String?,
+    @SerialName("DisplayOrder") val displayOrder: Int,
+    @SerialName("Owned") val owned: Boolean,
+    @SerialName("Selected") val selected: Boolean,
+    @SerialName("ShowInactive") val showInactive: Boolean
 )
 
 /**
@@ -149,39 +157,73 @@ data class Badge(
  *
  * @param value Karma name
  * */
+@Serializable
 enum class KarmaName(val value: String) {
+    @SerialName("aklıselim")
     Akliselim("aklıselim"),
+    @SerialName("anadolu çocuğu")
     AnadoluCocugu("anadolu çocuğu"),
+    @SerialName("anarşist")
     Anarsist("anarşist"),
+    @SerialName("ağır abi")
     AgirAbi("ağır abi"),
+    @SerialName("baldan tatlı")
     BaldanTatli("baldan tatlı"),
+    @SerialName("bal küpü")
     BalKupu("bal küpü"),
+    @SerialName("battal gazi")
     BattalGazi("battal gazi"),
+    @SerialName("bıçkın")
     Bickin("bıçkın"),
+    @SerialName("delikanlı")
     Delikanli("delikanlı"),
+    @SerialName("düz adam")
     DuzAdam("düz adam"),
+    @SerialName("energizer tavşanı")
     EnergizerTavsani("energizer tavşanı"),
+    @SerialName("gençlerin sevgilisi")
     GenclerinSevgilisi("gençlerin sevgilisi"),
+    @SerialName("her eve lazım")
     HerEveLazim("her eve lazım"),
+    @SerialName("hippi")
     Hippi("hippi"),
+    @SerialName("hırçın golcü")
     HircinGolcu("hırçın golcü"),
+    @SerialName("iç güveysinden hallice")
     IcGuveysindenHallice("iç güveysinden hallice"),
+    @SerialName("kaotik nötral")
     KaotikNotral("kaotik nötral"),
+    @SerialName("kızgın kumlardan serin sulara")
     KizginKumlardanSerinSulara("kızgın kumlardan serin sulara"),
+    @SerialName("kofti anarşist")
     KoftiAnarsist("kofti anarşist"),
+    @SerialName("leziz")
     Leziz("leziz"),
+    @SerialName("mangal yürekli rişar")
     MangalYurekliRisar("mangal yürekli rişar"),
+    @SerialName("minyonların sevgilisi")
     MinyonlarinSevgilisi("minyonların sevgilisi"),
+    @SerialName("mülayim ama sempatik")
     MulayimAmaSempatik("mülayim ama sempatik"),
+    @SerialName("müzmin yedek")
     MuzminYedek("müzmin yedek"),
+    @SerialName("padawan")
     Padawan("padawan"),
+    @SerialName("prezentabl")
     Prezentabl("prezentabl"),
+    @SerialName("rating canavarı")
     RatingCanavari("rating canavarı"),
+    @SerialName("tadına doyum olmaz")
     TadinaDoyumOlmaz("tadına doyum olmaz"),
+    @SerialName("çetrefilli")
     Cetrefilli("çetrefilli"),
+    @SerialName("çılgın")
     Cilgin("çılgın"),
+    @SerialName("şamda kayısı")
     SamdaKayisi("şamda kayısı"),
+    @SerialName("şekerpare")
     Sekerpare("şekerpare"),
+    @SerialName("şeker abi")
     SekerAbi("şeker abi"),
 }
 
@@ -190,246 +232,52 @@ enum class KarmaName(val value: String) {
  *
  * @param value Badge name
  * */
+@Serializable
 enum class BadgeName(val value: String) {
+    @SerialName("azimli")
     Azimli("azimli"),
+    @SerialName("bot")
     Bot("bot"),
+    @SerialName("cizreli")
     Cizreli("cizreli"),
+    @SerialName("cpu")
     Cpu("cpu"),
+    @SerialName("editör")
     Editor("editör"),
+    @SerialName("ekşi")
     Eksi("ekşi"),
+    @SerialName("ekşiteker")
     EksiTeker("ekşiteker"),
+    @SerialName("emekli")
     Emekli("emekli"),
+    @SerialName("gudik")
     Gudik("gudik"),
+    @SerialName("itfaiyeci")
     Itfaiyeci("itfaiyeci"),
+    @SerialName("karagöz")
     Karagoz("karagöz"),
+    @SerialName("kayıp")
     Kayip("kayıp"),
+    @SerialName("lanetli")
     Lanetli("lanetli"),
+    @SerialName("leyla")
     Leyla("leyla"),
+    @SerialName("merhum")
     Merhum("merhum"),
+    @SerialName("mücahit")
     Mucahit("mücahit"),
+    @SerialName("organizatör")
     Organizator("organizatör"),
+    @SerialName("ormancı")
     Ormanci("ormancı"),
+    @SerialName("preator")
     Preator("preator"),
+    @SerialName("scener")
     Scener("scener"),
+    @SerialName("tasnifçi")
     Tasnifci("tasnifçi"),
+    @SerialName("64-bit")
     The64Bit("64-bit"),
+    @SerialName("çaylak")
     Caylak("çaylak")
-}
-
-/**
- * Parses a JSON string to an [User] object
- *
- * @param json JSON string
- *
- * @return [User] object
- * */
-fun deserializeUser(json: String): User {
-    val jsonElement = Json.parseToJsonElement(json)
-
-    val userInfo = deserializeUserInfo(jsonElement.jsonObject["UserInfo"].toString())
-    val badges = jsonElement.jsonObject["Badges"]!!.jsonArray.map { deserializeBadge(it.toString()) }
-    val hasEntryUsedOnSeyler = jsonElement.jsonObject["HasEntryUsedOnSeyler"]!!.jsonPrimitive.boolean
-    val followerCount = jsonElement.jsonObject["FollowerCount"]!!.jsonPrimitive.int
-    val followingsCount = jsonElement.jsonObject["FollowingsCount"]!!.jsonPrimitive.int
-    val picture = jsonElement.jsonObject["Picture"]?.jsonPrimitive?.contentOrNull
-    val pinnedEntry =
-        if (jsonElement.jsonObject["PinnedEntry"] != JsonNull) deserializeTopic(jsonElement.jsonObject["PinnedEntry"].toString()) else null
-
-    return User(
-        userInfo = userInfo,
-        badges = badges,
-        hasEntryUsedOnSeyler = hasEntryUsedOnSeyler,
-        followerCount = followerCount,
-        followingsCount = followingsCount,
-        picture = picture,
-        pinnedEntry = pinnedEntry
-    )
-}
-
-/**
- * Parses a JSON string to an [UserIdentifier] object
- *
- * @param json JSON string
- *
- * @return [UserIdentifier] object
- * */
-fun deserializeUserIdentifier(json: String): UserIdentifier {
-    val jsonElement = Json.parseToJsonElement(json)
-
-    val nick = jsonElement.jsonObject["Nick"]!!.jsonPrimitive.content
-    val id = jsonElement.jsonObject["Id"]!!.jsonPrimitive.int
-
-    return UserIdentifier(
-        nick = nick,
-        id = id
-    )
-}
-
-/**
- * Parses a JSON string to an [UserInfo] object
- *
- * @param json JSON string
- *
- * @return [UserInfo] object
- * */
-fun deserializeUserInfo(json: String): UserInfo {
-    val jsonElement = Json.parseToJsonElement(json)
-
-    val userIdentifier = deserializeUserIdentifier(jsonElement.jsonObject["UserIdentifier"].toString())
-    val remainingInvitation = jsonElement.jsonObject["RemainingInvitation"]!!.jsonPrimitive.int
-    val twitterScreenName = jsonElement.jsonObject["TwitterScreenName"]?.jsonPrimitive?.contentOrNull
-    val facebookProfileUrl = jsonElement.jsonObject["FacebookProfileUrl"]?.jsonPrimitive?.contentOrNull
-    val facebookScreenName = jsonElement.jsonObject["FacebookScreenName"]?.jsonPrimitive?.contentOrNull
-    val instagramScreenName = jsonElement.jsonObject["InstagramScreenName"]?.jsonPrimitive?.contentOrNull
-    val instagramProfileUrl = jsonElement.jsonObject["InstagramProfileUrl"]?.jsonPrimitive?.contentOrNull
-    val karma =
-        if (jsonElement.jsonObject["Karma"] != JsonNull) deserializeKarma(jsonElement.jsonObject["Karma"].toString()) else null
-    val entryCounts = deserializeUserEntryCounts(jsonElement.jsonObject["EntryCounts"].toString())
-    val lastEntryDate =
-        if (jsonElement.jsonObject["LastEntryDate"] != JsonNull) LocalDateTime.parse(jsonElement.jsonObject["LastEntryDate"]!!.jsonPrimitive.content) else null
-    val standingQueueNumber = jsonElement.jsonObject["StandingQueueNumber"]!!.jsonPrimitive.int
-    val hasAnyPendingInvitation = jsonElement.jsonObject["HasAnyPendingInvitation"]!!.jsonPrimitive.boolean
-    val isBuddy = jsonElement.jsonObject["IsBuddy"]!!.jsonPrimitive.boolean
-    val isBlocked = jsonElement.jsonObject["IsBlocked"]!!.jsonPrimitive.boolean
-    val isFollowed = jsonElement.jsonObject["IsFollowed"]!!.jsonPrimitive.boolean
-    val isCorporate = jsonElement.jsonObject["IsCorporate"]!!.jsonPrimitive.boolean
-    val isDeactivated = jsonElement.jsonObject["IsDeactivated"]!!.jsonPrimitive.boolean
-    val isKarmaShown = jsonElement.jsonObject["IsKarmaShown"]!!.jsonPrimitive.boolean
-    val isCaylak = jsonElement.jsonObject["IsCaylak"]!!.jsonPrimitive.boolean
-    val isIndexTitlesBlocked = jsonElement.jsonObject["IsIndexTitlesBlocked"]!!.jsonPrimitive.boolean
-    val note = jsonElement.jsonObject["Note"]?.jsonPrimitive?.contentOrNull
-    val cursePeriod =
-        if (jsonElement.jsonObject["CursePeriod"] != JsonNull) deserializeCursePeriod(jsonElement.jsonObject["CursePeriod"].toString()) else null
-    val isCursed = jsonElement.jsonObject["IsCursed"]!!.jsonPrimitive.boolean
-    val isBanned = jsonElement.jsonObject["IsBanned"]!!.jsonPrimitive.boolean
-    val displayTwitterProfile = jsonElement.jsonObject["DisplayTwitterProfile"]!!.jsonPrimitive.boolean
-    val displayFacebookProfile = jsonElement.jsonObject["DisplayFacebookProfile"]!!.jsonPrimitive.boolean
-    val displayInstagramProfile = jsonElement.jsonObject["DisplayInstagramProfile"]!!.jsonPrimitive.boolean
-
-    return UserInfo(
-        userIdentifier = userIdentifier,
-        remainingInvitation = remainingInvitation,
-        twitterScreenName = twitterScreenName,
-        facebookProfileUrl = facebookProfileUrl,
-        facebookScreenName = facebookScreenName,
-        instagramScreenName = instagramScreenName,
-        instagramProfileUrl = instagramProfileUrl,
-        karma = karma,
-        entryCounts = entryCounts,
-        lastEntryDate = lastEntryDate,
-        standingQueueNumber = standingQueueNumber,
-        hasAnyPendingInvitation = hasAnyPendingInvitation,
-        isBuddy = isBuddy,
-        isBlocked = isBlocked,
-        isFollowed = isFollowed,
-        isCorporate = isCorporate,
-        isDeactivated = isDeactivated,
-        isKarmaShown = isKarmaShown,
-        isCaylak = isCaylak,
-        isIndexTitlesBlocked = isIndexTitlesBlocked,
-        note = note,
-        cursePeriod = cursePeriod,
-        isCursed = isCursed,
-        isBanned = isBanned,
-        displayTwitterProfile = displayTwitterProfile,
-        displayFacebookProfile = displayFacebookProfile,
-        displayInstagramProfile = displayInstagramProfile
-    )
-}
-
-/**
- * Parses a JSON string to an [CursePeriod] object
- *
- * @param json JSON string
- *
- * @return [CursePeriod] object
- * */
-fun deserializeCursePeriod(json: String): CursePeriod {
-    val jsonElement = Json.parseToJsonElement(json)
-
-    val from = LocalDateTime.parse(jsonElement.jsonObject["From"]!!.jsonPrimitive.content)
-    val to = LocalDateTime.parse(jsonElement.jsonObject["To"]!!.jsonPrimitive.content)
-
-    return CursePeriod(
-        from = from,
-        to = to
-    )
-}
-
-/**
- * Parses a JSON string to an [UserEntryCounts] object
- *
- * @param json JSON string
- *
- * @return [UserEntryCounts] object
- * */
-fun deserializeUserEntryCounts(json: String): UserEntryCounts {
-    val jsonElement = Json.parseToJsonElement(json)
-
-    val total = jsonElement.jsonObject["Total"]!!.jsonPrimitive.int
-    val lastMonth = jsonElement.jsonObject["LastMonth"]!!.jsonPrimitive.int
-    val lastWeek = jsonElement.jsonObject["LastWeek"]!!.jsonPrimitive.int
-    val today = jsonElement.jsonObject["Today"]!!.jsonPrimitive.int
-
-    return UserEntryCounts(
-        total = total,
-        lastMonth = lastMonth,
-        lastWeek = lastWeek,
-        today = today
-    )
-}
-
-/**
- * Parses a JSON string to an [Karma] object
- *
- * @param json JSON string
- *
- * @return [Karma] object
- *
- * @see KarmaName
- * */
-fun deserializeKarma(json: String): Karma {
-    val jsonElement = Json.parseToJsonElement(json)
-
-    val rawKarmaName = jsonElement.jsonObject["Name"]!!.jsonPrimitive.content
-    val karmaName = KarmaName.values().find { it.value == rawKarmaName }!!
-    val value = jsonElement.jsonObject["Value"]!!.jsonPrimitive.int
-
-    return Karma(
-        name = karmaName,
-        value = value
-    )
-}
-
-/**
- * Parses a JSON string to an [Badge] object
- *
- * @param json JSON string
- *
- * @return [Badge] object
- *
- * @see BadgeName
- * */
-fun deserializeBadge(json: String): Badge {
-    val jsonElement = Json.parseToJsonElement(json)
-
-    val id = jsonElement.jsonObject["Id"]!!.jsonPrimitive.int
-    val name = BadgeName.values().find { it.value == jsonElement.jsonObject["Name"]!!.jsonPrimitive.content }!!
-    val description = jsonElement.jsonObject["Description"]!!.jsonPrimitive.contentOrNull
-    val imageUrl = jsonElement.jsonObject["ImageUrl"]!!.jsonPrimitive.content
-    val displayOrder = jsonElement.jsonObject["DisplayOrder"]!!.jsonPrimitive.int
-    val owned = jsonElement.jsonObject["Owned"]!!.jsonPrimitive.boolean
-    val selected = jsonElement.jsonObject["Selected"]!!.jsonPrimitive.boolean
-    val showInactive = jsonElement.jsonObject["ShowInactive"]!!.jsonPrimitive.boolean
-
-    return Badge(
-        id = id,
-        name = name,
-        description = description,
-        imageUrl = imageUrl,
-        displayOrder = displayOrder,
-        owned = owned,
-        selected = selected,
-        showInactive = showInactive
-    )
 }
