8cb1b78fa1ca870cad97ec35ea412c70cc25d3ca
Onur Alp Akin <onralpakin@gmail.com>
2022-09-22 16:36:47 +0300

    add general response for user ops

---
+++ src/main/kotlin/com/github/otuva/eksisozluk/responses/GenericResponse.kt
@@ -0,0 +1,18 @@
+package com.github.otuva.eksisozluk.responses
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+/**
+ * Represents a general response from Eksi Sozluk API.
+ *
+ * @param success Whether the request was successful or not.
+ * @param message The message returned by the API.
+ * @param data The data returned by the API.
+ * */
+@Serializable
+open class GenericResponse(
+    @SerialName("Success") val success: Boolean,
+    @SerialName("Message") val message: String,
+    @SerialName("Data") val data: Unit?
+)
