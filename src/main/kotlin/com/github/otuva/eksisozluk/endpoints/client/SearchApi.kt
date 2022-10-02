df37a78ca27a6282f9420caf9862cef8b5fe4672
Onur Alp Akin <onralpakin@gmail.com>
2022-10-02 21:46:40 +0300

    add detailed search

--- src/main/kotlin/com/github/otuva/eksisozluk/endpoints/client/SearchApi.kt
+++ src/main/kotlin/com/github/otuva/eksisozluk/endpoints/client/SearchApi.kt
@@ -1,22 +1,54 @@
 package com.github.otuva.eksisozluk.endpoints.client
 
 import com.github.otuva.eksisozluk.endpoints.Routes
-import com.github.otuva.eksisozluk.models.authentication.UserType
+import com.github.otuva.eksisozluk.models.index.Index
 import com.github.otuva.eksisozluk.models.search.Autocomplete
+import com.github.otuva.eksisozluk.models.search.Search
+import com.github.otuva.eksisozluk.models.search.SortOrder
 import com.github.otuva.eksisozluk.responses.AutocompleteResponse
+import com.github.otuva.eksisozluk.responses.IndexResponse
 import com.github.otuva.eksisozluk.utils.urlEncode
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.request.*
+import io.ktor.http.*
+import kotlinx.datetime.LocalDateTime
 
-public class SearchApi(private val client: HttpClient, private val userType: UserType) {
+public class SearchApi(private val client: HttpClient) {
     public suspend fun autocomplete(query: String): Autocomplete {
-        val url = Routes.baseUrl + Routes.Search.autocomplete.format(urlEncode(query))
+        val url = Routes.api + Routes.Search.autocomplete.format(urlEncode(query))
 
         val response = client.get(url)
 
         val autocompleteResponse: AutocompleteResponse = response.body()
 
         return autocompleteResponse.data
+    }
+
+    public suspend fun search(
+        keywords: String,
+        from: LocalDateTime? = null,
+        to: LocalDateTime? = null,
+        author: String? = null,
+        sortOrder: SortOrder = SortOrder.ReverseChronological
+    ): Index {
+        val url = Routes.api + Routes.Search.search
+
+        val response = client.post(url) {
+            contentType(ContentType.Application.Json)
+            setBody(
+                Search(
+                    keywords,
+                    from,
+                    to,
+                    author,
+                    sortOrder
+                )
+            )
+        }
+
+        val searchResponse: IndexResponse = response.body()
+
+        return searchResponse.data
     }
 }
