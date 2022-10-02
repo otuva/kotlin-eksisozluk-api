68bcfa6f3dd6d7f6eb01746617c49c8374b51e1b
Onur Alp Akin <onralpakin@gmail.com>
2022-10-02 19:46:55 +0300

    remove unnecessary type parameter

--- src/main/kotlin/com/github/otuva/eksisozluk/endpoints/client/TopicApi.kt
+++ src/main/kotlin/com/github/otuva/eksisozluk/endpoints/client/TopicApi.kt
@@ -5,10 +5,10 @@ import com.github.otuva.eksisozluk.annotations.LimitedWithoutLogin
 import com.github.otuva.eksisozluk.annotations.RequiresLogin
 import com.github.otuva.eksisozluk.endpoints.Routes
 import com.github.otuva.eksisozluk.models.authentication.UserType
-import com.github.otuva.eksisozluk.models.search.TermType
 import com.github.otuva.eksisozluk.models.topic.SortingType
 import com.github.otuva.eksisozluk.models.topic.Topic
 import com.github.otuva.eksisozluk.responses.TopicResponse
+import com.github.otuva.eksisozluk.utils.urlEncode
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.request.*
@@ -52,24 +52,19 @@ public class TopicApi(private val client: HttpClient, private val userType: User
     /**
      * Search a term inside a topic | başlık içinde arama
      *
-     * To search user or a term inside topic you can use [termType] parameter. It's [TermType.Text] by default.
-     * Even though you can use [TermType.Entry] inside a topic, it won't search for entries, but instead it will search for given integer.
-     * However, you can use [TermType.Author] to search for a user.
+     * To search a user inside topic you can prefix it with '@' character.
      *
      * @param topicId The id of the topic
-     * @param termType The type of the term. Default is [TermType.Text]
      * @param searchTerm The term to search
      * @param page The page to get
      *
      * @return [Topic] object. Returned [Topic.entries] can be empty.
      * */
     @RequiresLogin
-    public suspend fun searchInTopic(topicId: Int, termType: TermType = TermType.Text, searchTerm: String, page: Int = 1): Topic {
+    public suspend fun searchInTopic(topicId: Int, searchTerm: String, page: Int = 1): Topic {
         check(userType == UserType.Regular) { NotAuthorizedException("Anonymous users cannot do this.") }
 
-        val finalSearchTerm = termType.value + searchTerm
-
-        val url = Routes.baseUrl + Routes.Topic.search.format(topicId, finalSearchTerm) + "?p=$page"
+        val url = Routes.baseUrl + Routes.Topic.search.format(topicId, urlEncode(searchTerm)) + "?p=$page"
 
         val response = client.get(url)
 
--- src/main/kotlin/com/github/otuva/eksisozluk/models/search/TermType.kt
+++
@@ -1,7 +0,0 @@
-package com.github.otuva.eksisozluk.models.search
-
-public enum class TermType(public val value: String) {
-    Text(""),
-    Entry("%23"),
-    Author("@"),
-}
