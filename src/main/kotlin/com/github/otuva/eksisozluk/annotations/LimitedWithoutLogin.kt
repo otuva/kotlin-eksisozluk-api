28fa85d4defd3c54b4d113e7ff7f3e46057dc033
Onur Alp Akin <onralpakin@gmail.com>
2022-10-08 19:11:12 +0300

    extend kdoc

--- src/main/kotlin/com/github/otuva/eksisozluk/annotations/LimitedWithoutLogin.kt
+++ src/main/kotlin/com/github/otuva/eksisozluk/annotations/LimitedWithoutLogin.kt
@@ -2,6 +2,8 @@ package com.github.otuva.eksisozluk.annotations
 
 /**
  * Annotation for methods that provides limited functionality without login.
+ *
+ * Things you can and cannot do are listed in the methods' documentation.
  * */
 @Retention(AnnotationRetention.BINARY)
 @Target(AnnotationTarget.FUNCTION)
