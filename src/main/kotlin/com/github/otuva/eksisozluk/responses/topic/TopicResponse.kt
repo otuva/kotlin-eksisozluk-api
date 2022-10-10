d3ebbe4d895aec9d3bd8267ed4d4e91a90b3151b
Onur Alp Akin <onralpakin@gmail.com>
2022-10-10 12:05:04 +0300

    remove requiring success

--- src/main/kotlin/com/github/otuva/eksisozluk/responses/topic/TopicResponse.kt
+++ src/main/kotlin/com/github/otuva/eksisozluk/responses/topic/TopicResponse.kt
@@ -1,6 +1,5 @@
 package com.github.otuva.eksisozluk.responses.topic
 
-import com.github.otuva.eksisozluk.BadTopicException
 import com.github.otuva.eksisozluk.models.topic.Topic
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
@@ -10,11 +9,4 @@ public data class TopicResponse(
     @SerialName("Success") val success: Boolean,
     @SerialName("Message") val message: String?,
     @SerialName("Data") val data: Topic?
-) {
-    /**
-     * This constructor conditional is for entries that are not found. Because api returns 200 status code for not found entries.
-     * */
-    init {
-        require(success) { throw BadTopicException(message!!) }
-    }
-}
+)
